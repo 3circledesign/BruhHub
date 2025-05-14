96e81bb807bde8ea41ef47cadc742a9a74e9f7c6
Gamebox <191238851+3circledesign@users.noreply.github.com>
2025-05-15 01:52:31 +0800

    Initial commit for 3330250

---
+++ 3330250.lua
@@ -0,0 +1,12 @@
+-- Steam App 3330250 Manifest
+-- Name: Ithya: Magic Studies
+-- Generated: 2025-05-14 12:50:29
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3330250) -- Ithya: Magic Studies
+
+-- MAIN APP DEPOTS
+addappid(3330251, 1, "a7633b3d30636dfe1dae9b0d4f95964133d913e1bc86fef04e6300f14129dca2") -- Main Game Content (Windows Content)
+setManifestid(3330251, "2157807002797518256", 0)
